699d9f1451e5f321c20b57f6ddee0b449b7677e9
CodeConnoisseur74 <sherry.andrews.com>
2024-05-18 23:58:34 +0200

    add js eventListener to connect now button

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,11 @@ menu.addEventListener("click", () => {
 });
 // End of Navbar
 
+// Connect Now Button 
+document.querySelector('.banner-btn').addEventListener('click', function () {
+    window.location.href = '#contact';
+});
+
 // Video Section
 const video = document.querySelector(".video");
 const btn = document.querySelector(".buttons i");
@@ -55,12 +60,4 @@ var swiper = new Swiper(".swiper", {
 });
 // End of Card Section
 
-// Footer Copyright
-// Create a new function called newDate()
-function newDate() {
-    //return a new Date() -- returns the current calendar year.
-    return new Date().getFullYear();
-}
-// after everything else has loaded on the page, load this command: find the element (like a <span>) with the ID of 'autoupdate' and insert the dash '-' and the result of the newDate() function that returns the current calendar year.  (ie. '-2017' or whatever the current calendar year is)
-document.onload = document.getElementById("autodate").innerHTML = '-' + newDate();
 
